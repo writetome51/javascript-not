7816718745349da8719474b988b74e40421d565c
Steve Thompson <writetome51@gmail.com>
2019-03-14 14:36:08 -0600

    Added one new test. Added a new example to README and cleaned up README. Upped version to 1.0.5

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
 # not(booleanExpression): boolean
 
 It's an alternative to writing `!(booleanExpression)`   
-This should be helpful to anyone who has problems spotting the sneaky `!` negation operator.  
-For me it's sometimes too tiny, too thin.
+This should be helpful to anyone who has problems spotting the sneaky `!` negation operator.
 
 ## Examples
 ```
 not(false); // --> true
-
-not(0); // --> true
 
 not(1 === 2); // --> true
 
 if ( not( isEmpty( [1,2,3,4] ) ) ) {
 	console.log('The array contains items');
 }
-  // --> console: 'The array contains items'
+  // console: 'The array contains items'
+  
+let x = 1, n = null;
+not(x === null || n !== null); // --> true
 ```
 
 ## Installation
-`npm i  @writetome51/not`
-(This has no dependencies, so it doesn't require npm to install.  You can download the zip file  
-from github if you want.)
+`npm i  @writetome51/not`  
+
 
 ## Loading
 ```
--- test.js
+++ test.js
@@ -2,22 +2,26 @@ var not = require('./index').not;
 
 
 if (not(false)) console.log('test 1 passed');
-else console.log('test 1 failed');
+else console.log('test 1 FAILED');
 
 if (not('')) console.log('test 2 passed');
-else console.log('test 2 failed');
+else console.log('test 2 FAILED');
 
 if (not(0)) console.log('test 3 passed');
-else console.log('test 3 failed');
+else console.log('test 3 FAILED');
 
 if (not(1 === 2))  console.log('test 4 passed');
-else console.log('test 4 failed');
+else console.log('test 4 FAILED');
 
-if (not('hello'))  console.log('test 5 failed');
+if (not('hello'))  console.log('test 5 FAILED');
 else console.log('test 5 passed');
 
-if (not([]))  console.log('test 6 failed');
+if (not([]))  console.log('test 6 FAILED');
 else console.log('test 6 passed');
 
-if (not('0')) console.log('test 7 failed');
+if (not('0')) console.log('test 7 FAILED');
 else console.log('test 7 passed');
+
+let n = null, x = 1;
+if (not(x === null || n !== null)) console.log('test 8 passed');
+else console.log('test 8 FAILED');
